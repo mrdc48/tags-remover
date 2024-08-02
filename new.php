193093820a311a38eb0166786eb77bfe2632a7b0
Chetan Dhotre <75079526+mrdc48@users.noreply.github.com>
2024-08-02 12:10:30 +0530

    Create new.php

---
+++ new.php
@@ -0,0 +1,56 @@
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_HD.png",SET HD
+https://yuvraj43.xyz/sliv/index.m3u8?ch=SETHD&id=2011671
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Max_HD.png",Sony MAX HD
+https://yuvraj43.xyz/sliv/index.m3u8?ch=SonyMaxhd&id=2011670
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Ten_HD.png",Sony TEN 1 HD
+https://yuvraj43.xyz/sliv/index.m3u8?ch=TEN1HD&id=2011747
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Ten2_HD.png",Sony TEN 2 HD
+https://yuvraj43.xyz/sliv/index.m3u8?ch=TEN2HD&id=2020434
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Ten3_HD.png",Sony TEN 3 HD
+https://yuvraj43.xyz/sliv/index.m3u8?ch=TEN3HD&id=2020591
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_Cineplex.png",Colors Cineplex HD
+https://prod-ent-live-gm.jiocinema.com/bpk-tv/JC_ColorsCineplexHD/JCHLS/index.m3u8
+#EXTINF:-1,Pictures  
+http://195.26.87.217:80/live.ts?channelId=139&uid=8084&deviceMac=00:1A:79:42:25:32
+#EXTINF:-1,Star Gold 2  
+http://195.26.87.217:80/live.ts?channelId=33165&uid=8084&deviceMac=00:1A:79:42:25:32
+#EXTINF:-1,Star Gold  
+http://195.26.87.217:80/live.ts?channelId=196&uid=8084&deviceMac=00:1A:79:42:25:32
+#EXTINF:-1,Zee Cinema HD
+http://195.26.87.217:80/live.ts?channelId=12359&uid=8084&deviceMac=00:1A:79:42:25:32
+#EXTINF:-1,Zee Action 
+http://195.26.87.217:80/live.ts?channelId=207&uid=8084&deviceMac=00:1A:79:42:25:32
+#EXTINF:-1,Zee Bollywood 
+http://195.26.87.217:80/live.ts?channelId=210&uid=8084&deviceMac=00:1A:79:42:25:32
+#EXTINF:-1,ZEE TALKIES  
+http://195.26.87.217:80/live.ts?channelId=28131&uid=8084&deviceMac=00:1A:79:42:25:32
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/ABP_Majha.png",Abp Majha
+https://abplivetv.akamaized.net/hls/live/2043011/manjha/master_720p.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/IBN_Lokmat.png",IBN lokmat
+https://prod-sports-hin-fa.jiocinema.com/bpk-tv/News18_Lokmat_voot_MOB/Fallback/index.m3u8
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB_F8WnNjrqSf400QvbzWS3t35pFKAsPCRS5kVHHBOW4xugFKRb4Fj8po&s=10",NDTV Marathi
+https://web-ndtv-marathi.akamaized.net/hls/live/2110470/ndtvmarathi/chunklist_0.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/TV9_Maharashtra.png",TV9 Marathi
+https://dyjmyiv3bp2ez.cloudfront.net/pub-iotv9marlygv8h/live_720p/chunks.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_24_Taas.png",Zee24 tass
+https://dgrvlduwztkd4.cloudfront.net/index_5.m3u8
+#EXTINF:-1 tvg-logo="https://smedia2.intoday.in/aajtak/aajtakhd/images/aajtakHd_logo.png",Aaj Tak HD
+https://aajtakhdlive-amd.akamaized.net/hls/live/2014415-b/aajtakhd/aajtakhdlive/live_1080p/chunks.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/ABP_News_India.png",Abp News 
+https://abplivetv.akamaized.net/hls/live/2043010/hindi/master_720.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/India_TV.png",india tv
+https://poclive-indiatvnews.akamaized.net/hlslive/Admin/px0219297/live/digital/chunklist_0.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/IBN_7.png",News18 india
+https://prod-sports-hin-fa.jiocinema.com/bpk-tv/News18_India_voot_MOB/Fallback/index.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_HD.png",Colors HD
+https://prod-ent-live-gm.jiocinema.com/bpk-tv/JC_ColorsHD/JCHLS/index.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_Marathi_HD.png",Colors Marathi HD
+https://prod-sports-hin-fa.jiocinema.com/bpk-tv/JC_ColorsMarathiHD/JCHLS/index.m3u8
+#EXTINF:-1,Sonic
+https://prod-ent-live-gm.jiocinema.com/bpk-tv/JC_SonicNick/JCHLS/index.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sports18_1_HD.png",SPORTS 18 HD
+https://prod-sports-hin-fa.jiocinema.com/bpk-tv/Sports18_1_HD_voot_MOB/Fallback/index.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sports18_Khel.png",Sports18 Khel
+https://prod-sports-hin-fa.jiocinema.com/bpk-tv/Sports18_Khel_voot_MOB/Fallback/index.m3u8
+#EXTINF:-1 tvg-logo="http://jiotv.catchup.cdn.jio.com/dare_images/images/MTV_Beats_HD.png",Mtv HD
+https://prod-sports-hin-fa.jiocinema.com/bpk-tv/MTV_Beats_HD_voot_MOB/Fallback/index.m3u8
